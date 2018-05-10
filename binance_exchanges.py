0aff804f53d935d4a414b2e83bec4509f8f66a97
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-05-10 10:48:56 +0900

    中身をbainaceに対応

--- binance_exchanges.py
+++ binance_exchanges.py
@@ -8,17 +8,17 @@ BINANCE = ccxt.bunance()# 取引所の指定
 
 def bitbank_id() -> " 取引所ID":
     """取引所IDを返します"""
-    return BITBANK.id
+    return BINANCE.id
 
 def bitbank_bid() -> " 購入価格":
     """購入価格を返します"""
-    bitbank_orderbook = BITBANK.fetch_order_book('XRP/JPY')
+    bitbank_orderbook = BINANCE.fetch_order_book('XRP/JPY')
     return bitbank_orderbook['bids'][0][0] if (bitbank_orderbook['bids']) > 0 else None
 
 
 def bitbank_ask() -> "売却価格":
     """売却価格を返します"""
-    bitbank_orderbook = BITBANK.fetch_order_book('XRP/JPY')
+    bitbank_orderbook = BINANCE.fetch_order_book('XRP/JPY')
     return bitbank_orderbook['asks'][0][0] if (bitbank_orderbook['asks']) > 0 else None
 
 
