24ecf1ea5b00e9e2f65274644a4d43ce5bbfb366
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-05-10 22:01:28 +0900

    全体のディレクトリの整理

--- app/XRPchart.py
+++ app/XRPchart.py
@@ -1,65 +1,55 @@
-import python_bitbankcc#インストールが必要
-import requests
-import json
+"""XRPの売値をccxtを使用して取得しそれを利用してチャートを表示する"""
 import time
 import datetime
+import ccxt
 import numpy as np#インストールが必要
 import matplotlib.pyplot as plt#インストールが必要
 
 #http://okuribitoni.hatenablog.com/entry/2018/01/11/211204を参考にした。
 
-sellPriceList = []
-listLength = 0
-maxlength = 100 #ﾁｬｰﾄx方向最大値です。
-frequency = 3 #ﾃﾞｰﾀ取得周期です。
-while True:
+PRICELIST = []
+LISTLENGTH = 0
+MAXLENGTH = 100 #ﾁｬｰﾄx方向最大値です。
+FREQUENCY = 1 #ﾃﾞｰﾀ取得周期です。
+EXCHANGE_LIST = ['bitbank',]#使用する取引所
 
-    #現在の時刻を取得
-    print(datetime.datetime.today())
+ASK_EXCHANGE = ''
+ASK_PRICE = 99999999
+BID_EXCHANGE = ''
+BID_PRICE = 99999999
+print(ccxt.exchanges)
 
-    pub = python_bitbankcc.public()
-    value = pub.get_ticker(
-        'xrp_jpy' # ペア
-    )
-    #print(value)
-
-    #現在の売り最安値 (sell)
-    #現在の買い最高値 (buy)
-    #過去24時間の最高値 (higt)
-    #過去24時間の最安値 (low)
+for exchange_id in EXCHANGE_LIST:
 
 
-    #からそれぞれ'現在の売り最安値 (sell)'を取得します。
-    sell = value['sell']
+    while True:
+        exchange = eval('ccxt.' + exchange_id + '()')
 
+        # symbol: 通貨ペア 例(XRP/JPYは1リップルは日本円でいくらになるか。)
+        orderbook = exchange.fetch_order_book(symbol='XRP/JPY')
+        #bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None  # 買い注文=bids
+        #print(bid)
+        ask = orderbook['asks'][0][0] if orderbook['asks'] is not None else None  # 売り注文=asks
+        print(ask)
 
-    if listLength<maxlength:
+        #現在の時刻を取得
+        print(datetime.datetime.today())
 
-        sellPriceList.append(sell)
-
-    else:
-        #list内の要素がmaxlengthを超えたら、
-        #先頭の1要素を削除し、最後尾に追加します。
-        sellPriceList.pop(0)
-        sellPriceList.append(sell)
-
-    #ﾁｬｰﾄ用ﾃﾞｰﾀの作成をします。
-    coincheck = np.array(sellPriceList)
-    listLength = len(sellPriceList)
-    x = np.linspace(0,listLength,listLength)
-
-    #ここからﾁｬｰﾄ作成です
-    plt.figure(1)
-    plt.plot(x,coincheck, label = 'xrp/jpy(sell)')
-    #plt.plot(x,zaif, label = 'xrp/jpyの現在の買い最高値')
-    plt.legend()
-    plt.pause(.001)
-    plt.clf()
-
-    time.sleep(frequency)
-
-    #毎秒取得
-    #time.sleep(1)
-
-
-
+        if LISTLENGTH < MAXLENGTH:
+            PRICELIST.append(ask)
+        else:
+            #list内の要素がmaxlengthを超えたら、
+            #先頭の1要素を削除し、最後尾に追加します。
+            PRICELIST.pop(0)
+            PRICELIST.append(ask)
+        #ﾁｬｰﾄ用ﾃﾞｰﾀの作成をします。
+        bitbankxrp = np.array(PRICELIST)
+        LISTLENGTH = len(PRICELIST)
+        x = np.linspace(0, LISTLENGTH, LISTLENGTH)
+        #ここからﾁｬｰﾄ作成です
+        plt.figure(1)
+        plt.plot(x, bitbankxrp, label='bitbank(xrp/jpy)')
+        plt.legend()
+        plt.pause(.001)
+        plt.clf()
+        time.sleep(FREQUENCY)
