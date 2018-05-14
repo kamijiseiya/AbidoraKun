a8c846985edae36b575f597ec3fca3dbf2710082
Tanigawa Hikaru <kbc16a13@stu.kawahara.ac.jp>
2018-05-14 13:36:13 +0900

    エエラーの解消

--- app/module/exchanges_binance.py
+++ app/module/exchanges_binance.py
@@ -13,15 +13,18 @@ def bitbank_id() -> " 取引所ID":
 def bitbank_bid() -> " 購入価格":
     """購入価格を返します"""
     bitbank_orderbook = BINANCE.fetch_order_book('XRP/JPY')
-    return bitbank_orderbook['bids'][0][0] if (bitbank_orderbook['bids']) > 0 else None
+    return bitbank_orderbook['bids'][0][0] if (bitbank_orderbook['bids'])else None
 
 
 def bitbank_ask() -> "売却価格":
     """売却価格を返します"""
     bitbank_orderbook = BINANCE.fetch_order_book('XRP/JPY')
-    return bitbank_orderbook['asks'][0][0] if (bitbank_orderbook['asks']) > 0 else None
+    return bitbank_orderbook['asks'][0][0] if (bitbank_orderbook['asks'])else None
 
 
 def bitbank_spread() -> "スプレッド":
     """スプレッド情報を返します"""
     return (bitbank_ask() - bitbank_bid()) if (bitbank_bid() and bitbank_ask()) else None
+
+if __name__ == "__main__": #テスト用
+    print(bitbank_bid) #xrp_btcの中身を表示する
