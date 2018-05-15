95bcec110a99047b45cfd182d82232be2bb9eb1f
Kota Tachibana <kbc16a12@stu.kawahara.ac.jp>
2018-05-15 13:40:47 +0900

    modify-btc_to_jpyの内容を作成。未テスト

--- app/module/btc_to_jpy.py
+++ app/module/btc_to_jpy.py
@@ -8,3 +8,11 @@ def btc_to_jpy(btc):
     '''
     BTCの枚数を引数として受け取り、日本円の金額に変換するメソッドです
     '''
+    #BTC/JPYのレートを取得する
+    bitbank = ccxt.bitbank() #bitbankの情報を呼び出す
+    orderbook = bitbank.fetch_order_book('BTC/JPY') #BTC/JPYのオーダーブック取得
+    #BTC/JPYのBIDを取得する
+    bid = orderbook['bids'][0][0] if (btc_jpy_orderbook['bids']) else None
+
+    jpy = btc / bid #BTCを日本円の額に変換する
+    return jpy
