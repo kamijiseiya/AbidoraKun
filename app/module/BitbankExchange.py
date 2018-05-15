fe8c835f77b2d524b1405f5a2c20f807badcfd06
Kota Tachibana <kbc16a12@stu.kawahara.ac.jp>
2018-05-15 14:43:27 +0900

    modify-BitbankExchangeを価格取得モジュールに対応

--- app/module/BitbankExchange.py
+++ app/module/BitbankExchange.py
@@ -3,17 +3,14 @@ Bitbankから取得したXRPをBTCに対応させるモジュールです
 '''
 #インポート
 import ccxt
+import exchanges_bitbank
 
 def exchange_xrp_btc_bid(xrp_jpy_bid):
     '''
     XRP/JPYのbidを引数で受け取り、XRP/BTCのbidに変換して返すメソッドです
     '''
-    #オーダーブック取得
-    bitbank = ccxt.bitbank() #bitbankの情報を呼び出す
-    btc_jpy_orderbook = bitbank.fetch_order_book('BTC/JPY') #BTC/JPYのオーダーブック取得
-
     #BTC/JPYのASKを取得する
-    btc_jpy_ask = btc_jpy_orderbook['asks'][0][0] if (btc_jpy_orderbook['asks']) else None
+    btc_jpy_ask = exchanges_bitbank.bitbank_btc_ask()
 
     #XRP/BTCのBIDを計算する
     xrp_btc_bid = xrp_jpy_bid / btc_jpy_ask if (xrp_jpy_bid and btc_jpy_ask) else None
@@ -26,12 +23,8 @@ def exchange_xrp_btc_ask(xrp_jpy_ask):
     '''
     XRP/JPYのaskを引数で受け取り、XRP/BTCのaskに変換して返すメソッドです
     '''
-    #オーダーブック取得
-    bitbank = ccxt.bitbank() #bitbankの情報を呼び出す
-    btc_jpy_orderbook = bitbank.fetch_order_book('BTC/JPY') #BTC/JPYのオーダーブック取得
-
     #BTC/JPYのBIDを取得する
-    btc_jpy_bid = btc_jpy_orderbook['bids'][0][0] if (btc_jpy_orderbook['bids']) else None
+    btc_jpy_bid = exchanges_bitbank.bitbank_btc_bid()
 
     #XRP/BTCのASKを計算する
     xrp_btc_ask = xrp_jpy_ask / btc_jpy_bid if (xrp_jpy_ask and btc_jpy_bid) else None
