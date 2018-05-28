cebd5af085c0d6178fc61836353e70a7882a786f
Tanigawa Hikaru <kbc16a13@stu.kawahara.ac.jp>
2018-05-28 11:25:25 +0900

    コードの修正

--- app/entry_screen.py
+++ app/entry_screen.py
@@ -1,106 +1,105 @@
-#ライブラリーはPythonで標準にあるtkinterを使用
-
+"""ユーザーのAPIキーをDBに登録する"""
 #インポート
 import sqlite3
-from tkinter import *
-from tkinter import ttk
+from tkinter import Tk, Label, Entry, END, Button
 
 # データベースファイルのパス(なければ作られる)
-dbpath = 'config/sample_db.sqlite'
+DBPATH = 'config/sample_db.sqlite'
 
 # データベース接続とカーソル生成
-connection = sqlite3.connect(dbpath)
+CONNECTION = sqlite3.connect(DBPATH)
 # 自動コミットにする場合は下記を指定（コメントアウトを解除のこと）
 # connection.isolation_level = None
-cursor = connection.cursor()
+CURSOR = CONNECTION.cursor()
 
 try:
     # CREATE  (IF NOT EXISTS　は作成済みのテーブルを作るエラーを解消)
     # execute (SQLを実行する)
-    cursor.execute(
+    CURSOR.execute(
         "CREATE TABLE IF NOT EXISTS bitbank (Private INTEGER PRIMARY KEY, Token TEXT)")
 
-    cursor.execute(
+    CURSOR.execute(
         "CREATE TABLE IF NOT EXISTS binace (Private INTEGER PRIMARY KEY, Token TEXT)")
     #エラー時の処理
 except sqlite3.Error as e:
     print('sqlite3.Error occurred:', e.args[0])
 
-root = Tk()
-root.title(u"Test")
-root.geometry("450x300")
+ROOT = Tk()
+ROOT.title(u"Test")
+ROOT.geometry("450x300")
 
-head = Label(text=u'API KEYS')
-head.grid(row=0, column=1)
-cond = Label(text=u'各取引所からAPIキーを取得してください。')
-cond.grid(row=1, column=1)
+HEAD = Label(text=u'API KEYS')
+HEAD.grid(row=0, column=1)
+CONTENT = Label(text=u'各取引所からAPIキーを取得してください。')
+CONTENT.grid(row=1, column=1)
 
-tori = Label(text=u'取引所')
-tori.grid(row=2, column=0)
+EXCHANGES = Label(text=u'取引所')
+EXCHANGES.grid(row=2, column=0)
 
-apik = Label(text=u'APIキー')
-apik.grid(row=2, column=1)
+APIK = Label(text=u'APIキー')
+APIK.grid(row=2, column=1)
 
-tokn = Label(text=u'トークンキー')
-tokn.grid(row=2, column=2)
+TOKEN = Label(text=u'トークンキー')
+TOKEN.grid(row=2, column=2)
 
-bank = Label(text=u'bitbank')
-bank.grid(row=3, column=0)
-bina = Label(text=u'binace')
-bina.grid(row=4, column=0)
+BANK = Label(text=u'bitbank')
+BANK.grid(row=3, column=0)
+BINA = Label(text=u'binace')
+BINA.grid(row=4, column=0)
 
 #エントリー　（privateキーの値を入れる)
-bitbank_api = Entry()
-bitbank_api.grid(row=3, column=1, padx=15)
+BITBANK_API = Entry()
+BITBANK_API.grid(row=3, column=1, padx=15)
 
-binace_api = Entry()
-binace_api.grid(row=4, column=1, padx=15)
+BINACE_API = Entry()
+BINACE_API.grid(row=4, column=1, padx=15)
 
 #エントリー２ (トークンの値を入れる)
-bitbank_token = Entry()
-bitbank_token.insert(END,"トークンの値")
-bitbank_token.grid(row=3, column=2, padx=20)
+BITBANK_TOKEN = Entry()
+BITBANK_TOKEN.insert(END, "トークンの値")
+BITBANK_TOKEN.grid(row=3, column=2, padx=20)
 
-binace_token = Entry()
-binace_token.insert(END,"トークンの値")
-binace_token.grid(row=4, column=2, padx=20)
+BINACE_TOKEN = Entry()
+BINACE_TOKEN.insert(END, "トークンの値")
+BINACE_TOKEN.grid(row=4, column=2, padx=20)
 
-def Bitbank_Value(event):
+def Bitbank_Value(entry):
     #エントリーの中身取得
-    API_value = bitbank_api.get()
-    token_value = bitbank_token.get()
+    BITBANK_VALUE_API = BITBANK_API.get()
+    BITBANK_VALUE_TOKEN = BITBANK_TOKEN.get()
     # INSERT (プレースホルダ使用)
-    cursor.execute("INSERT INTO bitbank VALUES (?, ?)", (API_value, token_value))
-    th = Label(text=u'bitbank登録しました。')
-    th.grid(row=5, column=1)
+    CURSOR.execute("INSERT INTO bitbank VALUES (?, ?)", (BITBANK_VALUE_API, BITBANK_VALUE_TOKEN))
+    BITBANK_ENTRY = Label(text=u'bitbank登録しました。')
+    BITBANK_ENTRY.grid(row=5, column=1)
 
 
 #ボタン(bitbank)
-Button_bitbank = Button(text=u'登録')
-Button_bitbank.bind("<Button-1>",Bitbank_Value)
-Button_bitbank.grid(row=3, column=4)
+BUTTON_BITBANK = Button(text=u'登録')
+BUTTON_BITBANK.bind("<Button-1>", Bitbank_Value)
+BUTTON_BITBANK.grid(row=3, column=4)
 
-def Binance_Value(event):
+def Binance_Value(entry):
     #エントリーの中身取得
-    API_value = binace_api.get()
-    token_value = binace_token.get()
+    BINACE_VALUE_API = BINACE_API.get()
+    BINACE_VALUE_TOKEN = BINACE_TOKEN.get()
     # INSERT (プレースホルダ使用)
-    cursor.execute("INSERT INTO binace VALUES (?, ?)", (API_value, token_value))
-    th.grid(row=6, column=1)
+    CURSOR.execute("INSERT INTO binace VALUES (?, ?)", (BINACE_VALUE_API, BINACE_VALUE_TOKEN))
+    BINACE_ENTRY = Label(text=u'binace登録しました。')
+    BINACE_ENTRY.grid(row=6, column=1)
 
 #ボタン(binace)
-Button_binace = Button(text=u'登録')
-Button_binace.bind("<Button-1>",Binance_Value)
-Button_binace.grid(row=4, column=4)
+BUTTON_BINACE = Button(text=u'登録')
+BUTTON_BINACE.bind("<Button-1>", Binance_Value)
+BUTTON_BINACE.grid(row=4, column=4)
 
 #ボタン(メインに戻る用)
-Main_Menu = Button(text=u'決定')
-Main_Menu.grid(row=7, column=1)
+MAIN_MENU = Button(text=u'決定')
+MAIN_MENU.grid(row=7, column=1)
 
-root.mainloop()
+ROOT.mainloop()
 
 # 保存を実行
-connection.commit()
+CONNECTION.commit()
 
 # 接続を閉じる
-connection.close()
+CONNECTION.close()
