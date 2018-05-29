7f964b4aaf736e3d2b6720515f65a4fc3c824eaf
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-05-29 14:24:08 +0900

    新規モジュールに対応

--- app/module/btc_to_jpy.py
+++ app/module/btc_to_jpy.py
@@ -8,13 +8,12 @@ def btc_to_jpy(btc):
     '''
     BTCの枚数を引数として受け取り、日本円の金額に変換するメソッドです
     '''
-    #BTC/JPYのレートを取得する
-    bitbank = ccxt.bitbank() #bitbankの情報を呼び出す
-    orderbook = bitbank.fetch_order_book('BTC/JPY') #BTC/JPYのオーダーブック取得
-    #BTC/JPYのBIDを取得する
-    bid = orderbook['bids'][0][0] if (orderbook['bids']) else None
 
-    jpy = btc * bid #BTCを日本円の額に変換する
+    BITBANKORDERBOOK = module.exchanges.bitbank.btc
+
+    bid = BITBANKORDERBOOK['bid'].get('bitbank_id')
+
+    jpy = btc * bid #BTCを日本円の額に変換する bitbank btc/jpy bidを呼び出す。
     return jpy
 
 if __name__ == "__main__": #テスト用
