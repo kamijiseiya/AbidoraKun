a8c278c098a744a4427320d26f43f34625057dcc
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-05-31 10:49:45 +0900

    pylintの点数上げ

--- app/module/Currency_exchange.py
+++ app/module/Currency_exchange.py
@@ -1,18 +1,18 @@
 """BTCからJPYへJPYからBTCに変換するモジュールです"""
 
 # インポート
-import ccxt
+
 # インポート
 from app import module
 
 
 def btc_to_jpy(btc):
-    '''
+    """
     BTCの枚数を引数として受け取り、日本円の金額に変換するメソッドです
-    '''
+    """
 
-    BITBANKORDERBOOK = module.exchanges.bitbank.btc(0)
-    bid = BITBANKORDERBOOK['bid'].get('bitbank')
+    bankbook = module.exchanges.bitbank.btc(0)
+    bid = bankbook['bid'].get('bitbank')
     jpy = btc * bid  # BTCを日本円の額に変換する bitbank btc/jpy bidを呼び出す。
     return jpy
 
@@ -20,46 +20,48 @@ def btc_to_jpy(btc):
 if __name__ == "__main__":  # テスト用
     print(btc_to_jpy(1))
 
+
+
 '''
-Bitbankから取得したXRP/JPYをBTCに対応させるモジュールです
+Bitandから取得したXRP/JPYをBTCに対応させるモジュールです
 '''
 
 
 def exchange_xrp_btc_bid(xrp_jpy_bid):
-    '''
+    """
     XRP/JPYのbidを引数で受け取り、XRP/BTCのbidに変換して返すメソッドです
-    '''
-    #BTC/JPYのASKを取得する
+    """
+    # BTC/JPYのASKを取得する
 
-    BITBANKORDERBOOK = module.exchanges.bitbank.btc(0)
+    BANKBOOK = module.exchanges.bitbank.btc(0)
 
-    btc_jpy_ask = BITBANKORDERBOOK['ask'].get('bitbank')
+    btc_jpy_ask = BANKBOOK['ask'].get('bitbank')
 
-    #XRP/BTCのBIDを計算する
+    # XRP/BTCのBIDを計算する
     xrp_btc_bid = xrp_jpy_bid / btc_jpy_ask if (xrp_jpy_bid and btc_jpy_ask) else None
 
-    #return
+    # return
     return xrp_btc_bid
 
 
 def exchange_xrp_btc_ask(xrp_jpy_ask):
-    '''
+    """
     XRP/JPYのaskを引数で受け取り、XRP/BTCのaskに変換して返すメソッドです
-    '''
-    #BTC/JPYのBIDを取得する
-    BITBANKORDERBOOK = module.exchanges.bitbank.btc(0)
+    """
+    # BTC/JPYのBIDを取得する
 
-    btc_jpy_bid = BITBANKORDERBOOK['bid'].get('bitbank')
+    bankbook = module.exchanges.bitbank.btc(0)
 
-    #XRP/BTCのASKを計算する
+    btc_jpy_bid = bankbook['bid'].get('bitbank')
+
+    # XRP/BTCのASKを計算する
     xrp_btc_ask = xrp_jpy_ask / btc_jpy_bid if (xrp_jpy_ask and btc_jpy_bid) else None
 
-    #return
+    # return
     return xrp_btc_ask
 
 
-
-if __name__ == "__main__": #テスト用
-    #結果の表示
+if __name__ == "__main__":  # テスト用
+    # 結果の表示
     print(exchange_xrp_btc_bid(800))
     print(exchange_xrp_btc_ask(80))
