b4ee9dd12da00d997a8b3eedc1769717144168e8
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-05-31 09:52:15 +0900

    モジュールをまとめた

--- app/module/Currency_exchange.py
+++ app/module/Currency_exchange.py
@@ -1,39 +1,44 @@
 """BTCからJPYへJPYからBTCに変換するモジュールです"""
 
-#インポート
+# インポート
 import ccxt
-#インポート
+# インポート
 from app import module
+
+
 def btc_to_jpy(btc):
     '''
     BTCの枚数を引数として受け取り、日本円の金額に変換するメソッドです
     '''
-    #BTC/JPYのレートを取得する
-    bitbank = ccxt.bitbank() #bitbankの情報を呼び出す
-    orderbook = bitbank.fetch_order_book('BTC/JPY') #BTC/JPYのオーダーブック取得
-    #BTC/JPYのBIDを取得する
+    # BTC/JPYのレートを取得する
+    bitbank = ccxt.bitbank()  # bitbankの情報を呼び出す
+    orderbook = bitbank.fetch_order_book('BTC/JPY')  # BTC/JPYのオーダーブック取得
+    # BTC/JPYのBIDを取得する
     bid = orderbook['bids'][0][0] if (orderbook['bids']) else None
 
-    jpy = btc * bid #BTCを日本円の額に変換する
+    jpy = btc * bid  # BTCを日本円の額に変換する
     return jpy
 
-if __name__ == "__main__": #テスト用
+
+if __name__ == "__main__":  # テスト用
     print(btc_to_jpy(1))
 
 '''
 Bitbankから取得したXRP/JPYをBTCに対応させるモジュールです
 '''
+
+
 def xrp_btc_bid(xrp_jpy_bid):
     '''
     XRP/JPYのbidを引数で受け取り、XRP/BTCのbidに変換して返すメソッドです
     '''
-    #BTC/JPYのASKを取得する
+    # BTC/JPYのASKを取得する
     btc_jpy_ask = exchanges_bitbank.bitbank_btc_ask()
 
-    #XRP/BTCのBIDを計算する
+    # XRP/BTCのBIDを計算する
     xrp_btc_bid = xrp_jpy_bid / btc_jpy_ask if (xrp_jpy_bid and btc_jpy_ask) else None
 
-    #return
+    # return
     return xrp_btc_bid
 
 
@@ -41,17 +46,17 @@ def xrp_btc_ask(xrp_jpy_ask):
     '''
     XRP/JPYのaskを引数で受け取り、XRP/BTCのaskに変換して返すメソッドです
     '''
-    #BTC/JPYのBIDを取得する
+    # BTC/JPYのBIDを取得する
     btc_jpy_bid = exchanges_bitbank.bitbank_btc_bid()
 
-    #XRP/BTCのASKを計算する
+    # XRP/BTCのASKを計算する
     xrp_btc_ask = xrp_jpy_ask / btc_jpy_bid if (xrp_jpy_ask and btc_jpy_bid) else None
 
-    #return
+    # return
     return xrp_btc_ask
 
 
-if __name__ == "__main__": #テスト用
-    #結果の表示
+if __name__ == "__main__":  # テスト用
+    # 結果の表示
     print(xrp_btc_bid(80))
     print(xrp_btc_ask(80))
