90966e1169e07b0d49a1a0dd39f8e25cff30f917
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2018-06-04 11:33:06 +0900

    test_exchanges.py import文修正

--- test/test_exchanges.py
+++ test/test_exchanges.py
@@ -1,21 +1,19 @@
 """exchanges.pyのテスト"""
 import unittest
-from app import module
-
-
+import app.module.exchanges as module
 class TestBitbank(unittest.TestCase):
     """exchanges.pyのテストクラス"""
 
     def test_returnbinancedata_id(self):
         """bainasuから値がとってこれているかどうか"""
-        testdata = module.exchanges.Binance.xrp(0)
+        testdata = module.Binance.xrp(0)
         print(testdata)
         print(testdata['binance'].get('binance_id'))
         self.assertEqual('binance', testdata['binance'].get('binance_id'))
 
     def test_bitbank_id_bitbank(self):
         """bitbankから値がとってこれているかどうか"""
-        bitbankdata = module.exchanges.Bitbank.xrp(0)
+        bitbankdata = module.Bitbank.xrp(0)
         print(bitbankdata['bitbank'].get('bitbank_id'))
         self.assertEquals('bitbank', bitbankdata['bitbank'].get('bitbank_id'))
 
