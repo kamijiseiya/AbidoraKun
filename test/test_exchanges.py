80f7c483cd737fa9c7b487cae97b3a60f7106626
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2018-06-04 10:39:20 +0900

    exchangess.pyの変更に伴い10行目のBINANCEをBinanceに、17行目のbitbankをBitbankに変更した。

--- test/test_exchanges.py
+++ test/test_exchanges.py
@@ -7,15 +7,15 @@ class TestBitbank(unittest.TestCase):
     # 値がとってこれるかどうか　エラーが発生するかどうか
     def test_returnbinancedata_id_binance(self):
         """bainasuから値がとってこれているかどうか"""
-        testdata = module.exchanges.BINANCE.xrp(0)
+        testdata = module.exchanges.Binance.xrp(0)
         print(testdata)
         print(testdata['binance'].get('binance_id'))
         self.assertEqual('binance', testdata['binance'].get('binance_id'))
 
     def test_bitbank_id_bitbank(self):
         """bitbankから値がとってこれているかどうか"""
-        bitbankdata = module.exchanges.bitbank.xrp(0)
-        print(bitbankdata)
+        bitbankdata = module.exchanges.Bitbank.xrp(0)
+        print(bitbankdata['bitbank'].get('bitbank_id'))
         self.assertEquals('bitbank' , bitbankdata['bitbank'].get('bitbank_id'))
 
 
