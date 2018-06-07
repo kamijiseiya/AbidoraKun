f21773d426163b7c9738b21081d32e7df3576f6c
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2018-06-07 15:00:26 +0900

    exchange_xrp_btc_bidとexchange_xrp_btc_askをexchange_xrp_btcにまとめた。

--- app/module/money_exchange/xrp_to_btc.py
+++ app/module/money_exchange/xrp_to_btc.py
@@ -3,39 +3,46 @@
 # インポート
 from app.module.exchangess import bitbank
 
+def exchange_xrp_btc(xrp_jpy, pricetype):
+    """XRP/JPYのpricetypeを引数で受け取り、XRP/BTCのpricetypeに変換して返すメソッドです"""
 
-def exchange_xrp_btc_bid(xrp_jpy_bid):
-    """XRP/JPYのbidを引数で受け取り、XRP/BTCのbidに変換して返すメソッドです"""
-
-    # BTC/JPYのASKを取得する
-
-    bitbankorderbook = bitbank.BITBANK.currencyinformation('BTC')
-
-    btc_jpy_ask = bitbankorderbook['ask'].get('bitbank')
-
-    # XRP/BTCのBIDを計算する
-    xrp_btc_bid = xrp_jpy_bid / btc_jpy_ask if (xrp_jpy_bid and btc_jpy_ask) else None
-
-    # return
-    return xrp_btc_bid
+    # pricetypeが'bid'の場合
+    if pricetype == 'bid':
 
 
-def exchange_xrp_btc_ask(xrp_jpy_ask):
-    """XRP/JPYのaskを引数で受け取り、XRP/BTCのaskに変換して返すメソッドです"""
+        # BTC/JPYのASKを取得する
 
-    # BTC/JPYのBIDを取得する
-    bitbankorderbook = bitbank.BITBANK.currencyinformation('BTC')
+        bitbankorderbook = bitbank.BITBANK.currencyinformation('BTC')
 
-    btc_jpy_bid = bitbankorderbook['bid'].get('bitbank')
+        btc_jpy_ask = price_acquisition('ask', bitbankorderbook)
 
-    # XRP/BTCのASKを計算する
-    xrp_btc_ask = xrp_jpy_ask / btc_jpy_bid if (xrp_jpy_ask and btc_jpy_bid) else None
+        # XRP/BTCのBIDを計算する
+        xrp_btc_bid = xrp_jpy / btc_jpy_ask if (xrp_jpy and btc_jpy_ask) else None
 
-    # return
-    return xrp_btc_ask
+        # return
+        return xrp_btc_bid
+
+    #pricetypeが'ask'の場合
+    elif pricetype == 'ask':
+        # BTC/JPYのBIDを取得する
+        bitbankorderbook = bitbank.BITBANK.currencyinformation('BTC')
+
+        btc_jpy_bid = price_acquisition('bid', bitbankorderbook)
+
+        # XRP/BTCのASKを計算する
+        xrp_btc_ask = xrp_jpy / btc_jpy_bid if (xrp_jpy and btc_jpy_bid) else None
+
+        # return
+        return xrp_btc_ask
+
+
+def price_acquisition(pricetype, orderbook):
+    """BTC/JPYのpricetypeで指定した引数をorderbookから取り出し、その値を返却する"""
+    return orderbook[pricetype].get('bitbank')
 
 
 if __name__ == "__main__":  # テスト用
     # 結果の表示
-    print(exchange_xrp_btc_bid(80))
-    print(exchange_xrp_btc_ask(80))
+
+    print(exchange_xrp_btc(80, 'bid'))
+    print(exchange_xrp_btc(80, 'ask'))
