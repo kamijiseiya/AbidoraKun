46c08ecb959694b37af635ff96f88e4ea41fcbff
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2018-06-07 10:13:41 +0900

    xrpとbtcをcurrencyinformationで処理できるように変更した

--- app/module/exchangess/bitbank.py
+++ app/module/exchangess/bitbank.py
@@ -7,12 +7,15 @@ import ccxt  # 取引所ライブラリをインポート
 class BITBANK:
     """bitbankからの取引データを処理するクラス"""
 
-    def btc(self):
+    def currencyinformation(self):
+        """bitbankのself(選択した通貨)/JPY取引データを返す"""
         while True:
             try:
-                bitbank = ccxt.bitbank()  # 取引所の指定
-                # biybankのXRP/JPYのオーダーブックの取得
-                bitbank_orderbook = bitbank.fetch_order_book('BTC/JPY')
+                bitbank = ccxt.bitbank()
+                # 通貨ペアself/JPYをcurrencypairで作成する
+                currencypair = self + '/JPY'
+                # biybankのcurrencypairのオーダーブックの取得
+                bitbank_orderbook = bitbank.fetch_order_book(currencypair)
                 # bitbank_orderbookからbidsの値を取得
                 bitbank_bid = bitbank_orderbook['bids'][0][0] \
                     if (bitbank_orderbook['bids']) else None
@@ -33,28 +36,11 @@ class BITBANK:
                 print("10秒待機してやり直します")
                 time.sleep(10)
 
-    def xrp(self):
-        while True:
-            try:
-                bitbank = ccxt.bitbank()  # 取引所の指定
-                # biybankのXRP/JPYのオーダーブックの取得
-                bitbank_orderbook = bitbank.fetch_order_book('XRP/JPY')
-                # bitbank_orderbookからbidsの値を取得
-                bitbank_bid = bitbank_orderbook['bids'][0][0] \
-                    if (bitbank_orderbook['bids']) else None
-                # bitbank_orderbookからasksの値を取得
-                bitbank_ask = bitbank_orderbook['asks'][0][0] \
-                    if (bitbank_orderbook['asks']) else None
 
-                orderbook = {'bitbank': {
-                    'bitbank_id': bitbank.id
-                }, 'bid': {
-                    bitbank.id: bitbank_bid
-                }, 'ask': {
-                    bitbank.id: bitbank_ask
-                }}
-                return orderbook
-            except ccxt.BaseError:
-                print("取引所から取引データを取得できません。")
-                print("10秒待機してやり直します")
-                time.sleep(10)
+
+
+
+Bitbank = BITBANK
+
+print(Bitbank.currencyinformation('BTC'))
+print(Bitbank.currencyinformation('XRP'))
