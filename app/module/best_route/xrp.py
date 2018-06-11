9d77b3422f6f7bc4646b6e0a62c81e3a470692f4
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-06-11 11:53:41 +0900

    feat:xrpの通貨裁定取引ルートの検索(未完成)

---
+++ app/module/best_route/xrp.py
@@ -0,0 +1,29 @@
+from app.module.exchangess import bitbank, binance
+
+ask_exhange = ''
+bid_exhange = ''
+bid_price = 0
+bitbank.BITBANK.xrp(0)
+bitbankdata = bitbank.BITBANK.xrp(0);
+bitbank_ask = bitbankdata['ask'].get('bitbank_id')
+bitbank_bid = bitbankdata['bid'].get('bitbank_id')
+binance.BINANCE.xrp(0)
+
+
+def ask_exhange(self):
+
+    else:bitbank('ask') > binance('ask'):
+
+
+def binance(bid,ask):
+    binancedaten = binance.BINANCE.xrp(0)
+
+    bid == binancedaten['bid'].get['binance_id']
+
+    ask == binancedaten['ask'].get['binance_id']
+
+def best_route():
+
+
+
+
