8c88355170687c82fce74d5f11e071b76903de90
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-06-12 09:30:01 +0900

    perf:最安値、最高値を求める式の追加

--- app/module/best_route/xrp.py
+++ app/module/best_route/xrp.py
@@ -2,28 +2,33 @@ from app.module.exchangess import bitbank, binance
 
 ask_exhange = ''
 bid_exhange = ''
+ask_price = 0
 bid_price = 0
-bitbank.BITBANK.xrp(0)
-bitbankdata = bitbank.BITBANK.xrp(0);
-bitbank_ask = bitbankdata['ask'].get('bitbank_id')
-bitbank_bid = bitbankdata['bid'].get('bitbank_id')
-binance.BINANCE.xrp(0)
+bitbank, bitbank_ask, bitbank_bid = bitbank.BITBANK.currencyinformation('XRP')
+binance, binance_ask, binance_bid = binance.BINANCE.xrp(0)
 
 
-def ask_exhange(self):
-
-    else:bitbank('ask') > binance('ask'):
-
-
-def binance(bid,ask):
-    binancedaten = binance.BINANCE.xrp(0)
-
-    bid == binancedaten['bid'].get['binance_id']
-
-    ask == binancedaten['ask'].get['binance_id']
-
-def best_route():
+def ask(self):
+    if bitbank_ask > binance_ask:
+        ask_price = binance_ask
+        ask_exhange = 'binance'
+        print(ask_exhange, ask_price, '取引A')
+    else:
+        ask_price = bitbank_ask
+        ask_exhange = 'bitbank'
+        print(ask_exhange, ask_price, '取引B')
 
 
+def bid(self):
+    if bitbank_bid > binance_bid:
+        bid_price = bitbank_bid
+        bid_exhange = 'bitbank'
+        print(bid_exhange, bid_price, '取引A')
+    else:
+        bid_price = binance_bid
+        bid_exhange = 'binance'
+        print(bid_exhange, bid_price, '取引B')
 
 
+if __name__ == "__main__":  # テスト用に追加
+    print(ask(0))
