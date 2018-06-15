eea5fbbda679d0027a3cb852ce10c0a54ae842b2
Kota Tachibana <kbc16a12@stu.kawahara.ac.jp>
2018-06-15 10:50:49 +0900

    fix-xrpchart.pyの処理を変更。不適切な変数名の修正。

--- app/xrpchart.py
+++ app/xrpchart.py
@@ -3,6 +3,7 @@ import time
 import datetime
 #import ccxt
 import numpy as np#インストールが必要
+import pandas as pd#インストールが必要
 import matplotlib.pyplot as plt#インストールが必要
 from module import exchanges_bitbank
 from module import exchanges_binance
@@ -10,60 +11,58 @@ from module import btc_to_jpy
 
 #http://okuribitoni.hatenablog.com/entry/2018/01/11/211204を参考にした。
 
-BITBANKPRICELIST = []
-BITBANKLISTLENGTH = 0
-BINANCEPRICELIST = []
-BINANCELISTLENGTH = 0
+list_bitbank_price = []
+list_length_bitbank = 0
+list_binance_price = []
+list_length_binance = 0
 MAXLENGTH = 100 #ﾁｬｰﾄx方向最大値です。
 FREQUENCY = 10 #ﾃﾞｰﾀ取得周期です。
 
 #print(ccxt.exchanges)
 
 while True:
-    # BITBANKでのXRP売値=BITBANKASK
-    BITBANKASK = exchanges_bitbank.bitbank_ask()\
+    # BITBANKでのXRP売値=bitbank_ask
+    bitbank_ask = exchanges_bitbank.bitbank_ask()\
         if exchanges_bitbank.bitbank_ask() is not None else None
 
-    BINANCEASK = btc_to_jpy.btc_to_jpy(exchanges_binance.binace_ask())
-    print(BITBANKASK)
-    print(BINANCEASK)
+    binance_ask = btc_to_jpy.btc_to_jpy(exchanges_binance.binace_ask())
+    print(bitbank_ask)
+    print(binance_ask)
 
     # 現在の時刻を取得
     print(datetime.datetime.today())
     time.sleep(FREQUENCY)
 
-    if BITBANKLISTLENGTH < MAXLENGTH and BINANCELISTLENGTH < MAXLENGTH:
-        BITBANKPRICELIST.append(BITBANKASK)
-        BINANCEPRICELIST.append(BINANCEASK)
+    if list_length_bitbank < MAXLENGTH and list_length_binance < MAXLENGTH:
+        list_bitbank_price.append(bitbank_ask)
+        list_binance_price.append(binance_ask)
     else:
         # list内の要素がmaxlengthを超えたら、
         # 先頭の1要素を削除し、最後尾に追加します。
-        if BITBANKLISTLENGTH < MAXLENGTH:
-            BITBANKPRICELIST.append(BITBANKASK)
-            BINANCEPRICELIST.pop(0)
-            BINANCEPRICELIST.append(BINANCEASK)
-        elif BINANCELISTLENGTH < MAXLENGTH:
-            BINANCEPRICELIST.append(BINANCEASK)
-            BITBANKPRICELIST.pop(0)
-            BITBANKPRICELIST.append(BITBANKASK)
+        if list_length_bitbank < MAXLENGTH:
+            list_bitbank_price.append(bitbank_ask)
+            list_binance_price.pop(0)
+            list_binance_price.append(binance_ask)
+        elif list_length_binance < MAXLENGTH:
+            list_binance_price.append(binance_ask)
+            list_bitbank_price.pop(0)
+            list_bitbank_price.append(bitbank_ask)
         else:
-            BITBANKPRICELIST.pop(0)
-            BITBANKPRICELIST.append(BITBANKASK)
-            BINANCEPRICELIST.pop(0)
-            BINANCEPRICELIST.append(BINANCEASK)
+            list_bitbank_price.pop(0)
+            list_bitbank_price.append(bitbank_ask)
+            list_binance_price.pop(0)
+            list_binance_price.append(binance_ask)
     # ﾁｬｰﾄ用ﾃﾞｰﾀの作成をします。
-    BITBANKXRP = np.array(BITBANKPRICELIST)
-    BITBANKLISTLENGTH = len(BITBANKPRICELIST)
-    BITBANKX = np.linspace(0, BITBANKLISTLENGTH, BITBANKLISTLENGTH)
+    bitbank_xrp = pd.Series(list_bitbank_price)
+    list_length_bitbank = len(list_bitbank_price)
 
-    BINANCEXRP = np.array(BINANCEPRICELIST)
-    BINANCELISTLENGTH = len(BINANCEPRICELIST)
-    BINANCEX = np.linspace(0, BINANCELISTLENGTH, BINANCELISTLENGTH)
+    binance_xrp = pd.Series(list_binance_price)
+    list_length_binance = len(list_binance_price)
 
     # ここからﾁｬｰﾄ作成です
     plt.figure(1)
-    plt.plot(BITBANKX, BITBANKXRP, label='bitbank(xrp/jpy)')
-    plt.plot(BINANCEX, BINANCEXRP, label='binance(xrp/jpy)')
+    bitbank_xrp.plot(label='bitbank(xrp/jpy)')
+    binance_xrp.plot(label='binance(xrp/jpy)')
     plt.legend()  # チャート名表示
     plt.pause(.001)  # チャート画面を表示
     plt.clf()
