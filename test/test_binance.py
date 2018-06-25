a79e9b0a604c222fb370192b792c892bf2273a1d
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2018-06-25 11:53:02 +0900

    test:test_binance.pyのエラーC: 8, 0: Constant name exhanges doesn't conform to UPPER_CASE naming style (invalid-name) 、C: 8,10: Constant name ask doesn't conform to UPPER_CASE naming style (invalid-name) 、C: 8,15: Constant name bid doesn't conform to UPPER_CASE naming style (invalid-name)修正

--- test/test_binance.py
+++ test/test_binance.py
@@ -1,20 +1,20 @@
 """exchanges.pyのテスト"""
 import os  # パスを操作するモジュール
+import unittest
 import sys  # パスを読み込むモジュール
 sys.path.append(os.path.abspath(os.path.join('..')))  # 自作モジュールのパス指定
-import unittest
 from app.module.exchangess import binance
 
-exhanges, ask, bid = binance.BINANCE.xrp(0)
+EXHANGES, ASK, BID = binance.BINANCE.xrp(0)
 class TestBitbank(unittest.TestCase):
     """bainasu.pyのテストクラス"""
 
     def test_returnbinancedata_id(self):
         """bainasuから値がとってこれているかどうか"""
 
-        print(exhanges, ask, bid)
-        print(exhanges)
-        self.assertEqual('binance', exhanges)
+        print(EXHANGES, ASK, BID)
+        print(EXHANGES)
+        self.assertEqual('binance', EXHANGES)
 
 
     def test_get_address_btc_address(self):
