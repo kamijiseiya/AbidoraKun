02a937d599898647b5a0e64608c517290c4f58ee
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-06-28 14:01:03 +0900

    style:pylintの点数上げに伴いスタイルの変更

--- app/module/sns/line.py
+++ app/module/sns/line.py
@@ -1,9 +1,10 @@
+"""" LINEを操作するモジュール"""
 # Python 3.5.2 にて動作を確認
 # sqlite3 標準モジュールをインポート
 import sqlite3
 
 # データベースファイルのパス
-DBPATH = 'sample_db.sqlite'
+DBPATH = 'cash_cow_db.sqlite'
 
 # データベース接続とカーソル生成
 CONNECTION = sqlite3.connect(DBPATH)
@@ -13,30 +14,29 @@ CURSOR = CONNECTION.cursor()
 
 
 # エラー処理（例外処理）
+
 class LINE:
+    """" LINEを操作するクラス"""
 
     def registration(name, api):
+        """" APIkキーを登録するメソッド"""
         try:
 
-            CURSOR.execute("DROP TABLE IF EXISTS sample")
+            CURSOR.execute("DROP TABLE IF EXISTS sns")
             CURSOR.execute(
-                "CREATE TABLE IF NOT EXISTS sample (name TEXT PRIMARY KEY, api TEXT)")
+                "CREATE TABLE IF NOT EXISTS sns (name TEXT PRIMARY KEY, api TEXT)")
             # INSERT
-            CURSOR.execute("INSERT INTO sample VALUES (:name, :api)",
-                           {'name': name, 'api':  api})
-            # プレースホルダの使用例
-            # プレースホルダには疑問符(qmark スタイル)と名前(named スタイル)の2つの方法がある
-            # 1つの場合には最後に , がないとエラー。('鈴木') ではなく ('鈴木',)
-            CURSOR.execute("INSERT INTO sample VALUES ('LINE', 'kpksdkふぉsじょそぼsmvsd「')")
+            CURSOR.execute("INSERT INTO sns VALUES (:name, :api)",
+                           {'name': name, 'api': api})
+
+            CURSOR.execute('SELECT * FROM sns ORDER BY name')
+
             # 保存を実行（忘れると保存されないので注意）
             CONNECTION.commit()
             # 接続を閉じる
             CONNECTION.close()
+            # 登録された値を返す
             return api, name
-        except sqlite3.Error as e:
-            print('sqlite3.Error occurred:', e.args[0])
+        except sqlite3.Error as error:
+            print('sqlite3.Error occurred:', error.args[0])
             return 'none'
-
-
-if __name__ == "__main__":  # テスト用に追加
-    print(LINE.registration('slack' , '上地'))
