130a7941c4278f33c679834cad9c05663c3bf8d4
kamijiseiya <kbc16a04@stu.kawahara.ac.jp>
2018-07-08 14:22:34 +0900

    feat:apiキーを保存する機能の追加

--- app/module/sns/line.py
+++ app/module/sns/line.py
@@ -2,6 +2,8 @@
 # Python 3.5.2 にて動作を確認
 # sqlite3 標準モジュールをインポート
 import sqlite3
+import requests
+import re  # 正規表現を使用するため
 
 # データベースファイルのパス
 DBPATH = 'cash_cow_db.sqlite'
@@ -28,8 +30,6 @@ class LINE:
             # INSERT
             CURSOR.execute("INSERT INTO sns VALUES (:name, :api)",
                            {'name': name, 'api': api})
-            CURSOR.execute('SELECT * FROM sns ORDER BY name')
-            res = CURSOR.fetchall()
 
             # 保存を実行（忘れると保存されないので注意）
             CONNECTION.commit()
@@ -39,19 +39,20 @@ class LINE:
             return api, name
         except sqlite3.Error as error:
             print('sqlite3.Error occurred:', error.args[0])
-            return 'none'
+            return None
 
     def search_apykey(name):
         try:
             CURSOR.execute("SELECT api FROM sns where  name like" + "'"+name+"'")
-
-            res = CURSOR.fetchall()
-            print(res)
+            """正規表現で形を整える"""
+            TOKEN_REWORK = re.sub('\)|\(|\,|\'', '', str(CURSOR.fetchall()))
+            print(TOKEN_REWORK)
             CONNECTION.close()
-            return res
+            return TOKEN_REWORK
         except sqlite3.Error as error:
             print('sqlite3.Error occurred:', error.args[0])
-        return 'none'
+        return None
 if __name__ == "__main__":  # テスト用に追加
     #print(LINE.registration('test','0001'))
     print(LINE.search_apykey('test'))
+    #print(LINE.search_apykey('test'))
