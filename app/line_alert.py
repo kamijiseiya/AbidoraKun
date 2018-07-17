2aefbb6e32b15cf2c88589565bfe155696acdbcf
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2018-07-17 14:08:12 +0900

    feat :line_imageメソッドにLineへメッセージ(今から画像を送ります。)を通知するように変更した。

--- app/line_alert.py
+++ app/line_alert.py
@@ -45,7 +45,17 @@ class LINE_ALERT:
     @staticmethod
     def line_image():
         """Lineに画像を送るためのサンプル"""
-        pass
+        while True:
+            url = "https://notify-api.line.me/api/notify"
+            token = 'LINE Notifyのアクセストークン'# DBから取得予定
+            headers = {"Authorization": "Bearer " + token}
+            message = ("今から画像を送ります。")
+            params = {"message": message}
+            post = requests.post(url, headers=headers, params=params)
+            print(post.status_code)  # ステータスコード取得
+            # １時間ごとに取得
+            time.sleep(3600)
+            return post.status_code
 
 print(LINE_ALERT.line())
 
