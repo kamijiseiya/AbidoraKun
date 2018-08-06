d1a37f20970feff9d414974bc35bf1e914677a0a
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2018-08-06 13:41:14 +0900

    test:test_line_image_not_exist(line_imageメソッドが存在しないファイルを指定した場合グラフが存在しません。と返却するか)のテストを追加

--- test/test_line.py
+++ test/test_line.py
@@ -25,3 +25,11 @@ class Testline(unittest.TestCase):
         decision = line.LINE.line_image("../app/config/img/figure.png")
         print(decision)
         self.assertEqual("今からグラフを送ります。", decision)
+
+
+    def  test_line_image_not_exist(self):
+        """line.pyのline_imageメソッドのテスト(存在しないファイルを指定した場合グラフが存在しません。と返却するか)"""
+
+        decision = line.LINE.line_image("../app/config/img/kakakak.png")
+        print(decision)
+        self.assertEqual("グラフが存在しません。", decision)
