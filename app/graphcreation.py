5dde990c0002af632f96f0d5c4b4d9be6b5217c7
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2019-01-18 13:44:15 +0900

    perf:引数名変更

--- app/graphcreation.py
+++ app/graphcreation.py
@@ -1,28 +1,27 @@
-import numpy as np
-import pandas as pd
-import json
-import requests
 from datetime import datetime
-from pprint import pprint
-import time
-
-import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 
-#------------------------------------------
-def sma(df, n):
-    df = df.rolling(window=int(n)).mean()
-    return df
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
 
-def ewm(df, n):
-    df = df.ewm(span=int(n)).mean()
-    return df
+#------------------------------------------
+def sma(Df, Number):
+    Df = Df.rolling(window=int(Number)).mean()
+    return Df
+
+def ewm(Df, Number):
+    Df = Df.ewm(span=int(Number)).mean()
+    return Df
 
 #設定   ------------------------
-GET_PERIODS = 60 * 5 # 時間足(秒)
-GET_AFTER = 50 # 足本数
+GET_PERIODS = 60 * 5
+# 時間足(秒)
+GET_AFTER = 50
+# 足本数
 
 class GRAPHCREATION:
+    # グラフを作成するクラス
     def create_graph_png(self):
         # Cryptowatch API History Data   ------------------------
         now = datetime.now()
@@ -31,20 +30,20 @@ class GRAPHCREATION:
         get_periods = GET_PERIODS
         get_after = now - get_periods * GET_AFTER
         # HTTPライブラリ Requestsによる価格履歴取得
-        r = requests.get(
+        req = requests.get(
             "https://api.cryptowat.ch/markets/" + market + "/ohlc?periods=" + str(get_periods) + "&after=" + str(
                 get_after)).json()
         print("[ now :", now, "]", "[ periods :", get_periods, "]", "[ after :", get_after, "]")
         # Pandas OHLCデータ   ------------------------
-        ohlcv = r["result"][str(get_periods)]
+        ohlcv = req["result"][str(get_periods)]
         df = pd.DataFrame(ohlcv, columns=["datetime", "open", "high", "low", "close", "volume", ''])
         df = df.set_index("datetime")
 
         # Indicator   ------------------------
         slider1_min, slider1_max, slider1_init, slider1_step = 1, 100, 15, 1
-        slider2_min, slider2_max, slider2_init, slider2_step = 1, 100, 5, 1
+        # slider2_min, slider2_max, slider2_init, slider2_step = 1, 100, 5, 1
         df_1 = sma(df["close"], slider1_init)
-        df_2 = ewm(df["close"], slider1_init)
+        # df_2 = ewm(df["close"], slider1_init)
 
         print(df.head(5))
         print(df.iloc[len(df_1) - 1])
@@ -53,12 +52,12 @@ class GRAPHCREATION:
         plt.style.use("ggplot")  # matplotlib ggplot style読込
 
         plt.rcParams['figure.figsize'] = 9.7, 3.7  # 横970px、縦370px
-        gs = GridSpec(4, 1, height_ratios=[6, 3, 0.2, 1])  # 縦４個、横１個のグリッドとその表示比率
-        gs.update(left=0.1, bottom=0, right=0.95, top=0.95, hspace=0, )  # 全体の余白
+        gridspecs = GridSpec(4, 1, height_ratios=[6, 3, 0.2, 1])  # 縦４個、横１個のグリッドとその表示比率
+        gridspecs.update(left=0.1, bottom=0, right=0.95, top=0.95, hspace=0, )  # 全体の余白
 
         # 表示する線の描画
 
-        ax0 = plt.subplot(gs[0])
+        ax0 = plt.subplot(gridspecs[0])
 
 
         ax0.plot(df["close"], marker="o", markersize=3, linewidth=2, color="orange", label="close")
@@ -71,7 +70,7 @@ class GRAPHCREATION:
         ax0.tick_params(axis="x", bottom=False, labelbottom=False)  # x軸目盛とラベルの非表示
         # ax0.axhline(0, color="orange")
 
-        ax1 = plt.subplot(gs[1], sharex=ax0)
+        ax1 = plt.subplot(gridspecs[1], sharex=ax0)
         ax1.spines["top"].set_color("slategray")  # ax0とax1の境界線
         ax1.bar(df.index, df["volume"], color="orange", width=get_periods * 0.94, label="volume")
         ax1.legend()
