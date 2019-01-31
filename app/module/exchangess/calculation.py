849dc6fc77fc2ad551ad9236f77f6f7a539ffda4
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2019-01-31 15:06:30 +0900

    perf:bitbank_binances_xrpメソッドをdifference_xrpメソッド(取引所間でのxrp差額を求めるメソッド)に変更

--- app/module/exchangess/calculation.py
+++ app/module/exchangess/calculation.py
@@ -5,23 +5,53 @@ class CALCULATION:
     """取引所間の通貨差額を求めるクラス"""
 
     def difference_xrp(self):
-        
+        """取引所間でのxrp差額を求めるメソッド
+                    (bitbank,binance,coinex)"""
+        # bitbankのXRP/BTCのaskとbidを算出
         bitbanks = ccxt.bitbank()
         bitbank_btc_jpy = bitbanks.fetch_ticker('BTC/JPY')
         bitbank_xrp_jpy = bitbanks.fetch_ticker('XRP/JPY')
         bitbank_xrp_btc_ask = bitbank_xrp_jpy.get("bid") / bitbank_btc_jpy.get("ask")
         bitbank_xrp_btc_bid = bitbank_xrp_jpy.get("ask") / bitbank_btc_jpy.get("bid")
+
+        # binanceからXRP/BTC通貨情報取得
         binances = ccxt.binance()
         binance_xrp_btc = binances.fetch_ticker('XRP/BTC')
-        profit_bitbank = bitbank_xrp_btc_ask - binance_xrp_btc.get("bid")
-        profit_binance = binance_xrp_btc.get("ask") - bitbank_xrp_btc_bid
+
+        # coinexからXRP/BTC通貨情報取得
+        coinex = ccxt.coinex()
+        coinex_xrp_btc = coinex.fetch_ticker('XRP/BTC')
+        print(coinex_xrp_btc.get("bid"))
+        print(coinex_xrp_btc.get("ask"))
+
+        # bitbankとbinance間の差額
+        profit_bitbank_binance = bitbank_xrp_btc_ask - binance_xrp_btc.get("bid")
+        profit_binance_bitbank = binance_xrp_btc.get("ask") - bitbank_xrp_btc_bid
+
+        # bitbankとcoinex間の差額
+        profit_bitbank_coinex = bitbank_xrp_btc_ask - coinex_xrp_btc.get("bid")
+        profit_coinex_bitbank = coinex_xrp_btc.get("ask") - bitbank_xrp_btc_bid
+
+        # binanceとcoinex間の差額
+        profit_binance_coinex = binance_xrp_btc.get("ask") - coinex_xrp_btc.get("bid")
+        profit_coinex_binance = coinex_xrp_btc.get("ask") - binance_xrp_btc.get("bid")
 
         print('XRPをbitbankで買いbinancesに売った場合の利益(btc):')
-        print(profit_bitbank)
+        print(profit_bitbank_binance)
         print('XRPをbinancesで買いbitbankに売った場合の利益(btc):')
-        print(profit_binance)
-        return profit_bitbank, profit_binance
+        print(profit_binance_bitbank)
+        print('XRPをbitbankで買いcoinexに売った場合の利益(btc)')
+        print(profit_bitbank_coinex)
+        print('XRPをcoinexで買いbitbankに売った場合の利益(btc)')
+        print(profit_coinex_bitbank)
+        print('XRPをbinanceで買いcoinexに売った場合の利益(btc)')
+        print(profit_binance_coinex)
+        print('XRPをcoinexで買いbinanceに売った場合の利益(btc)')
+        print(profit_coinex_binance)
+
+        return profit_bitbank_binance, profit_binance_bitbank, profit_bitbank_coinex, \
+               profit_coinex_bitbank, profit_binance_coinex, profit_coinex_binance
 
 
 if __name__ == "__main__":
-    print(CALCULATION.bitbank_binances_xrp(""))
+    print(CALCULATION.difference_xrp(""))
