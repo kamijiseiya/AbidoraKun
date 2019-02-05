b4699da78dd9ed00429ee5396a4777e9242b9fba
yamanakayuji <kbc16a21@stu.kawahara.ac.jp>
2019-02-05 10:12:11 +0900

    perf:difference_btc_xrpメソッドにXRPで通貨ペアの差額情報を戻り値とする処理を追加

--- app/module/exchangess/calculation.py
+++ app/module/exchangess/calculation.py
@@ -65,8 +65,8 @@ class CALCULATION:
                 bitbanks = ccxt.bitbank()
                 bitbank_btc_jpy = bitbanks.fetch_ticker('BTC/JPY')
                 bitbank_xrp_jpy = bitbanks.fetch_ticker('XRP/JPY')
-                bitbank_xrp_btc_ask = bitbank_xrp_jpy.get("bid") / bitbank_btc_jpy.get("ask")
-                bitbank_xrp_btc_bid = bitbank_xrp_jpy.get("ask") / bitbank_btc_jpy.get("bid")
+                bitbank_btc_xrp_ask = (1/bitbank_xrp_jpy.get("bid")) / (1/bitbank_btc_jpy.get("ask"))
+                bitbank_btc_xrp_bid = (1/bitbank_xrp_jpy.get("ask")) / (1/bitbank_btc_jpy.get("bid"))
 
                 # binanceからXRP/BTC通貨情報取得
                 binances = ccxt.binance()
@@ -76,18 +76,33 @@ class CALCULATION:
                 coinex = ccxt.coinex()
                 coinex_xrp_btc = coinex.fetch_ticker('XRP/BTC')
 
+
                 # bitbankとbinance間の差額
-                profit_bitbank_binance = bitbank_xrp_btc_ask - binance_xrp_btc.get("bid")
-                profit_binance_bitbank = binance_xrp_btc.get("ask") - bitbank_xrp_btc_bid
+                profit_bitbank_binance = bitbank_btc_xrp_ask - (1/binance_xrp_btc.get("bid"))
+                profit_binance_bitbank = (1/binance_xrp_btc.get("ask")) - bitbank_btc_xrp_bid
 
                 # bitbankとcoinex間の差額
-                profit_bitbank_coinex = bitbank_xrp_btc_ask - coinex_xrp_btc.get("bid")
-                profit_coinex_bitbank = coinex_xrp_btc.get("ask") - bitbank_xrp_btc_bid
+                profit_bitbank_coinex = bitbank_btc_xrp_ask - (1/coinex_xrp_btc.get("bid"))
+                profit_coinex_bitbank = (1/coinex_xrp_btc.get("ask")) - bitbank_btc_xrp_bid
 
                 # binanceとcoinex間の差額
-                profit_binance_coinex = binance_xrp_btc.get("ask") - coinex_xrp_btc.get("bid")
-                profit_coinex_binance = coinex_xrp_btc.get("ask") - binance_xrp_btc.get("bid")
-                return bitbank_xrp_btc_ask
+                profit_binance_coinex = (1/binance_xrp_btc.get("ask")) - (1/coinex_xrp_btc.get("bid"))
+                profit_coinex_binance = (1/coinex_xrp_btc.get("ask")) - (1/binance_xrp_btc.get("bid"))
+                # 'XRPを取引した場合の最大利益(xrp):'
+                maxvalue = max([profit_bitbank_binance, profit_binance_bitbank, profit_bitbank_coinex,
+                                profit_coinex_bitbank, profit_binance_coinex, profit_coinex_binance])
+                # 'XRPを取引した場合の最低利益(xrp):'
+                minvalue = min([profit_bitbank_binance, profit_binance_bitbank, profit_bitbank_coinex,
+                                profit_coinex_bitbank, profit_binance_coinex, profit_coinex_binance])
+
+                resultarray = {'bitbank_binance': profit_bitbank_binance,
+                               'binance_bitbank': profit_binance_bitbank,
+                               'bitbank_coinex': profit_bitbank_coinex,
+                               'coinex_bitbank': profit_coinex_bitbank,
+                               'binance_coinex': profit_binance_coinex,
+                               'coinex_binance': profit_coinex_binance,
+                               'max': maxvalue, 'min': minvalue}
+                return resultarray
             except ccxt.BaseError:
                 print("取引所から取引データを取得できません。")
                 print("10秒待機してやり直します")
